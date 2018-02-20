4b338fde32dea1f2435d782080715b225432a945
mimita3635 <Zinat36@gmail.com>
2018-02-20 23:53:37 +0600

    Uploaded sample file
    Added function FI

---
+++ Sample.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author user
+ */
+public class Sample {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int Fi(int a){
+        return a;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
