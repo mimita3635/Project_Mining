536d0e360a9ff372e5628134480ae700594003d0
mimita3635 <Zinat36@gmail.com>
2018-03-01 19:48:37 +0600

    New file added

---
+++ Sample2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author user
+ */
+public class Sample2 {
+     int Fifi(int a,int b){
+        return a;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
